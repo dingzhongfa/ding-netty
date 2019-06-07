79cb49c4a81ec93688ef3f9e255833956ce19ca3
丁太石 <71964899@qq.com>
2019-06-07 11:33:06 +0800

    demo2

---
+++ src/main/java/com/ding/netty/demo2/server/TimerServer.java
@@ -0,0 +1,40 @@
+package com.ding.netty.demo2.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimerServer {
+
+    private void bind(int port) throws InterruptedException {
+        // 配置服务端的线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).
+                    option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChildChannelHandler());
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new TimerServer().bind(8080);
+    }
+}
